c8c6cbfa47643945f4775d594bea298c0432c424
Siddhartha Mishra <siddharthamishra@Siddharthas-MacBook-Air.local>
2024-04-22 02:43:32 +0530

    web socket.io

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(_dirname, "frontend", "build", "index.html"));
+});
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    //console.log(data);      
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
